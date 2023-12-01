1fcad71c36662f5c308ae5da224fcc8615a64858
furkancak1r <furkancakr7@gmail.com>
2023-12-01 13:24:06 +0300

    6. Commit - 1.12.2023
    Path and value changes.

--- Excel_Data_Matching_and_Copy.py
+++ Excel_Data_Matching_and_Copy.py
@@ -2,7 +2,7 @@ import os
 import win32com.client
 # Logo'dan fixidesk'e eşleşen fatura numarası ve tarihleri yazdırmak için kullanıldı
 # Specify the file path
-file_path = "C:\\Users\\furkan.cakir\\Desktop\\FurkanPRS\\Kodlar\\Python_Excel_Data_Matching_Copy\\old.xlsx"
+file_path = "C:\\Users\\furkan.cakir\\Desktop\\FurkanPRS\\Kodlar\\SSH\\SSH Exceller\\old.xlsx"
 
 # Check if the file exists
 if os.path.exists(file_path):
@@ -17,7 +17,7 @@ if os.path.exists(file_path):
         # Read data into dictionaries for fixidesk and logo columns
         fixidesk_data = {}
         logo_data = {}
-        fixidesk_range = worksheet.Range("A:A")
+        fixidesk_range = worksheet.Range("F:F")
         logo_range = result_sheet.Range("D:D")
         for i, fixidesk_value in enumerate(fixidesk_range.Value):
             fixidesk_data[fixidesk_value] = i + 1
@@ -29,9 +29,9 @@ if os.path.exists(file_path):
             if fixidesk_value in logo_data:
                 logo_row = logo_data[fixidesk_value]
                 # Get the value from logo B and write it to fixidesk C
-                worksheet.Cells(fixidesk_row, 3).Value = result_sheet.Cells(logo_row, 2).Value
+                worksheet.Cells(fixidesk_row, 8).Value = result_sheet.Cells(logo_row, 2).Value
                 # Get the value from logo C and write it to fixidesk D
-                worksheet.Cells(fixidesk_row, 4).Value = result_sheet.Cells(logo_row, 3).Value
+                worksheet.Cells(fixidesk_row, 7).Value = result_sheet.Cells(logo_row, 1).Value
 
         # Save and close the workbook
         workbook.Save()
